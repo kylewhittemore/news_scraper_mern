b76fb10e58c54fb8d13c3e7d7a7321e46f358ff9
KDW <kyle.whittemore@gmail.com>
2019-07-28 21:41:05 -0400

    populate article with comments

--- models/articleModel.js
+++ models/articleModel.js
@@ -17,7 +17,15 @@ let Article = new Schema({
     byline: {
         type: String,
         required: true
-    }
+    },
+    comments: [
+        {
+          // Store ObjectIds in the array
+          type: Schema.Types.ObjectId,
+          // The ObjectIds will refer to the ids in the Note model
+          ref: "Coment"
+        }
+      ]
 });
 
 module.exports = mongoose.model('Article', Article)
