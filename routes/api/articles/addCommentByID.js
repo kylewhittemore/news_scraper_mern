73dd623a65ea049be8cae6b9c9ce29ba89e0a020
KDW <kyle.whittemore@gmail.com>
2019-07-31 22:07:26 -0400

    added put route for article "isFavorite"

--- routes/api/articles/addCommentByID.js
+++ routes/api/articles/addCommentByID.js
@@ -4,7 +4,7 @@ let Comment = require('../../../models/commentModel')
 module.exports = (req, res) => {
     Comment.create(req.body)
         .then(dbComment => {
-            return Article.findOneAndUpdate({ _id: req.params.id }, { $push: { comments: dbComment._id }}, { new: true })
+            return Article.findByIdAndUpdate(req.params.id, { $push: { comments: dbComment._id }}, { new: true })
         })
         .then(dbArticle => res.json(dbArticle))
         .catch(err => res.json(err))
--- routes/api/articles/index.js
+++ routes/api/articles/index.js
@@ -1,7 +1,8 @@
 const articles = require('express').Router();
 const all = require('./getAll');
 const getById = require('./getByID');
-const updateById = require('./updateByID');
+const addCommentById = require('./addCommentByID');
+const updateArticleById = require('./updateArticleById');
 
 // Tested wroking
 articles.get('/', all);
@@ -10,6 +11,8 @@ articles.get('/', all);
 articles.get('/:id', getById)
 
 // Tested Working
-articles.post('/:id', updateById)
+articles.post('/:id', addCommentById)
+
+articles.put('/:id', updateArticleById)
 
 module.exports = articles;
